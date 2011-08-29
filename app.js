975ef43b7c3cd160c11b8258fe7089850f321285
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-08-28 21:51:47 -0500

    changed app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (function() {
-  var app, fs, handler, io;
+  var app, fs, handler, io, port;
   handler = function(req, res) {
     return fs.readFile(__dirname + "/index.html", function(err, data) {
       if (err) {
-        res.writeHead(500);
+        return res.writeHead(500);
         return res.end("Error loading index.html");
       }
       res.writeHead(200);
@@ -13,7 +13,10 @@
   app = require("http").createServer(handler);
   io = require("socket.io").listen(app);
   fs = require("fs");
-  app.listen(1337);
+  port = process.env.PORT || 1337;
+  app.listen(port, function() {
+    return console.log("Listening on " + port);
+  });
   io.sockets.on("connection", function(socket) {
     socket.emit("news", {
       hello: "world"
