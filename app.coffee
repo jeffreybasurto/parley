d3a1af164c7addfbbb0c67d9de24ab33f02b4542
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-08-28 23:27:56 -0500

    added postgres adapter

--- app.coffee
+++ app.coffee
@@ -1,12 +1,34 @@
 express = require 'express'
+http = require 'http'
+port = process.env.PORT || 1337;
 app = express.createServer express.logger()
 
 app.set 'view engine', 'eco'
 app.set 'view options', { layout: false }
 
+app.post "/message", (request, response) ->
+  console.log("Posting!!!!")
+  response.send request.body
 
 app.get '/', (request, response) -> 
   response.render 'index'
+
+app.get '/test', (request, response) ->
+  options = {
+    host: 'localhost',
+    port: port,
+    path: '/message',
+    method: 'POST'
+  }
+  req = http.request options, (res) -> 
+    res.setEncoding('utf8');
+    res.on 'data', (chunk) ->
+      console.log 'BODY: ' + chunk
+
+  req.write 'data\n'
+  req.write 'data\n'
+  req.end()
+  response.send "Thanks."
   
 io = require("socket.io").listen(app)
 fs = require("fs")
@@ -15,11 +37,10 @@ io.configure () ->
   io.set "transports", ["xhr-polling"] 
   io.set "polling duration", 10
 
-port = process.env.PORT || 1337;
 app.listen port, () -> 
   console.log("Listening on " + port);
 
 io.sockets.on "connection", (socket) ->
-  socket.emit "news", hello: "world"
+  socket.emit "update", messages: "1337"
   socket.on "my other event", (data) ->
     console.log data
--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 (function() {
-  var app, express, fs, io, port;
+  var app, express, fs, http, io, port;
   express = require('express');
+  http = require('http');
+  port = process.env.PORT || 1337;
   app = express.createServer(express.logger());
   app.set('view engine', 'eco');
   app.set('view options', {
     layout: false
   });
+  app.post("/message", function(request, response) {
+    console.log("Posting!!!!");
+    return response.send(request.body);
+  });
   app.get('/', function(request, response) {
     return response.render('index');
+  });
+  app.get('/test', function(request, response) {
+    var options, req;
+    options = {
+      host: 'localhost',
+      port: port,
+      path: '/message',
+      method: 'POST'
+    };
+    req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      return res.on('data', function(chunk) {
+        return console.log('BODY: ' + chunk);
+      });
+    });
+    req.write('data\n');
+    req.write('data\n');
+    req.end();
+    return response.send("Thanks.");
   });
   io = require("socket.io").listen(app);
   fs = require("fs");
@@ -15,13 +40,12 @@
     io.set("transports", ["xhr-polling"]);
     return io.set("polling duration", 10);
   });
-  port = process.env.PORT || 1337;
   app.listen(port, function() {
     return console.log("Listening on " + port);
   });
   io.sockets.on("connection", function(socket) {
-    socket.emit("news", {
-      hello: "world"
+    socket.emit("update", {
+      messages: "1337"
     });
     return socket.on("my other event", function(data) {
       return console.log(data);
